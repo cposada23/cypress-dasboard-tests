1adfe28defe929f134c50af7e0afa7460a1de1ba
camiloesub <105243064+camiloesub@users.noreply.github.com>
2022-07-02 16:41:30 -0500

    Modify project id

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   video: true,
   screenshotOnRunFailure: false,
-  projectId: '97nm7q',
+  projectId: 'i7f4ow',
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
