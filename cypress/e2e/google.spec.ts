75c0f0885110b5b8dd0d0bca3a9639e3ada83c82
camiloesub <105243064+camiloesub@users.noreply.github.com>
2022-07-03 12:03:19 -0500

    one fails

--- cypress/e2e/google.spec.ts
+++ cypress/e2e/google.spec.ts
@@ -2,7 +2,8 @@ describe('search id', () => {
   it('should have a title with id', () => {
     cy.visit('https://www.google.com/');
     cy.title().should('eq', 'Google');
-    cy.log(Cypress.env('CONTAINER') || 'NO_CONTAINER');
+    cy.get('[name="q"]').type(Cypress.env('container') || 'NO_CONTAINER');
     cy.get('[name="q"]').type(Cypress.env('PROJECT_ID') || 'NO_PROJECT_ID');
+    cy.should('be.true', false);
   });
 });
