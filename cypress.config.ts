c9e99cf6063bbb421be4e54a7b187c34fe1cbac0
camiloesub <105243064+camiloesub@users.noreply.github.com>
2022-07-03 10:31:19 -0500

    pid

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   video: true,
   screenshotOnRunFailure: false,
+  // projectId: 'i7f4ow',
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
