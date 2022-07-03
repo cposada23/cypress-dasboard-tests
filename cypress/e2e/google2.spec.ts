2276c1eac08c16a5aaa25714b25c32611f7e3601
camiloesub <105243064+camiloesub@users.noreply.github.com>
2022-07-02 23:32:50 -0500

    tooojgyfh

--- cypress/e2e/google2.spec.ts
+++ cypress/e2e/google2.spec.ts
@@ -3,8 +3,11 @@ describe('This is my first cypress test', () => {
     cy.visit('https://www.google.com/');
     cy.title().should('eq', 'Google');
     cy.log(Cypress.env('CONTAINER') || 'NO_CONTAINER');
-    cy.get('[name="q"]').type(Cypress.env('CONTAINER') || 'NO_CONTAINER');
-    cy.get('[name="q"]').type(Cypress.env('NUMBER') || 'NONUMBER');
-    cy.get('[name="q"]').type(Cypress.env('TEST') || 'NOTEST');
+
+    cy.get('[name="q"]').type(`
+      ${Cypress.env('CONTAINER') || 'NO_CONTAINER'} 
+      ${Cypress.env('NUMBER') || 'NONUMBER'} 
+      ${Cypress.env('TEST') || 'NOTEST'}
+    `);
   });
 });
