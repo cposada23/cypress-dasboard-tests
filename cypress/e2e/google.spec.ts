058664a70c177a1c96d8a4421d2228059b136134
camiloesub <105243064+camiloesub@users.noreply.github.com>
2022-07-03 10:35:16 -0500

    pidsecrets

--- cypress/e2e/google.spec.ts
+++ cypress/e2e/google.spec.ts
@@ -3,6 +3,6 @@ describe('This is my first cypress test', () => {
     cy.visit('https://www.google.com/');
     cy.title().should('eq', 'Google');
     cy.log(Cypress.env('CONTAINER') || 'NO_CONTAINER');
-    cy.get('[name="q"]').type(Cypress.env('CONTAINER') || 'NO_CONTAINER');
+    cy.get('[name="q"]').type(Cypress.env('PROJECT_ID') || 'NO_PROJECT_ID');
   });
 });
