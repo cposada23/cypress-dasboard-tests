edf6c2c4d16605fe11254e546934d09fe9851671
camiloesub <105243064+camiloesub@users.noreply.github.com>
2022-07-02 19:30:51 -0500

    remove

--- cypress/e2e/google3.spec.ts
+++ cypress/e2e/google3.spec.ts
@@ -2,9 +2,8 @@ describe('This is my first cypress test', () => {
   it('should have a title', () => {
     cy.visit('https://www.google.com/');
     cy.title().should('eq', 'Google');
-    cy.log(Cypress.env('CONTAINER') || 'NO_CONTAINER');
+    cy.log(Cypress.env('COMMIT_INFO_MESSAGE') || 'NO_CONTAINER');
     cy.get('[name="q"]').type(Cypress.env('CONTAINER') || 'NO_CONTAINER');
-    cy.get('input[type="submit"]').click();
     // eslint-disable-next-line cypress/no-unnecessary-waiting
     cy.wait(2000);
   });
