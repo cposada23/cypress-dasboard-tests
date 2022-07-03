da20c6f3193164922059e62996bfbbb926f82507
camiloesub <105243064+camiloesub@users.noreply.github.com>
2022-07-02 19:21:54 -0500

    Test matrix

--- cypress/e2e/google.spec.ts
+++ cypress/e2e/google.spec.ts
@@ -1,7 +1,11 @@
 describe('This is my first cypress test', () => {
   it('should have a title', () => {
     cy.visit('https://www.google.com/');
-    cy.log(Cypress.env('CONTAINER') || 'NO_CONTAINER');
     cy.title().should('eq', 'Google');
+    cy.log(Cypress.env('CONTAINER') || 'NO_CONTAINER');
+    cy.get('[name="q"]').type(Cypress.env('CONTAINER') || 'NO_CONTAINER');
+    cy.get('input[type="submit"]').click();
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(2000);
   });
 });
