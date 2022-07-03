148e157a948a71fac7dbc2221f9b6db9338065d3
camiloesub <105243064+camiloesub@users.noreply.github.com>
2022-07-03 15:12:39 -0500

    in its own path?

--- cypress/e2e/google2.spec.ts
+++ cypress/e2e/google2.spec.ts
@@ -12,17 +12,4 @@ describe('title', () => {
     cy.get('[name="q"]').type(Cypress.env('USER') || 'NOUSER');
     cy.get('[name="q"]').type(Cypress.env('ORG_ID') || 'NOORG_ID');
   });
-  it('should have a title', () => {
-    cy.visit('/');
-    cy.title().should('eq', 'Google');
-    cy.log(Cypress.env('CONTAINER') || 'NO_CONTAINER');
-    // eslint-disable-next-line cypress/no-unnecessary-waiting
-    cy.wait(10000);
-    cy.get('[name="q"]').type(Cypress.env('container') || 'NO_CONTAINER');
-    cy.get('[name="q"]').type(Cypress.env('NUMBER') || 'NO_NUMBER');
-    cy.get('[name="q"]').type(Cypress.env('TEST') || 'NOTEST');
-    cy.get('[name="q"]').type(Cypress.env('apiPort') || 'NOapiPort');
-    cy.get('[name="q"]').type(Cypress.env('USER') || 'NOUSER');
-    cy.get('[name="q"]').type(Cypress.env('ORG_ID') || 'NOORG_ID');
-  });
 });
--- cypress/e2e/google3.spec.ts
+++ cypress/e2e/google3.spec.ts
@@ -1,12 +1,4 @@
 describe('search id', () => {
-  it('should have a title with id', () => {
-    cy.visit('https://www.google.com/');
-    // eslint-disable-next-line cypress/no-unnecessary-waiting
-    cy.wait(10000);
-    cy.title().should('eq', 'Google');
-    cy.get('[name="q"]').type(Cypress.env('container') || 'NO_CONTAINER');
-    cy.get('[name="q"]').type(Cypress.env('PROJECT_ID') || 'NO_PROJECT_ID');
-  });
   it('should have a title with failure', () => {
     cy.visit('https://www.google.com/');
     // eslint-disable-next-line cypress/no-unnecessary-waiting
