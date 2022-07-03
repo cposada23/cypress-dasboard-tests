1cf036a6edf3ed569568e0853e2247e77fb2c0d8
camiloesub <105243064+camiloesub@users.noreply.github.com>
2022-07-02 20:44:05 -0500

    add print?

--- cypress/e2e/google2.spec.ts
+++ cypress/e2e/google2.spec.ts
@@ -7,36 +7,4 @@ describe('This is my first cypress test', () => {
     // eslint-disable-next-line cypress/no-unnecessary-waiting
     cy.wait(2000);
   });
-  it('should have a title', () => {
-    cy.visit('https://www.google.com/');
-    cy.title().should('eq', 'Google');
-    cy.log(Cypress.env('CONTAINER') || 'NO_CONTAINER');
-    cy.get('[name="q"]').type(Cypress.env('CONTAINER') || 'NO_CONTAINER');
-    // eslint-disable-next-line cypress/no-unnecessary-waiting
-    cy.wait(2000);
-  });
-  it('should have a title', () => {
-    cy.visit('https://www.google.com/');
-    cy.title().should('eq', 'Google');
-    cy.log(Cypress.env('CONTAINER') || 'NO_CONTAINER');
-    cy.get('[name="q"]').type(Cypress.env('CONTAINER') || 'NO_CONTAINER');
-    // eslint-disable-next-line cypress/no-unnecessary-waiting
-    cy.wait(2000);
-  });
-  it('should have a title', () => {
-    cy.visit('https://www.google.com/');
-    cy.title().should('eq', 'Google');
-    cy.log(Cypress.env('CONTAINER') || 'NO_CONTAINER');
-    cy.get('[name="q"]').type(Cypress.env('CONTAINER') || 'NO_CONTAINER');
-    // eslint-disable-next-line cypress/no-unnecessary-waiting
-    cy.wait(2000);
-  });
-  it('should have a title', () => {
-    cy.visit('https://www.google.com/');
-    cy.title().should('eq', 'Google');
-    cy.log(Cypress.env('CONTAINER') || 'NO_CONTAINER');
-    cy.get('[name="q"]').type(Cypress.env('CONTAINER') || 'NO_CONTAINER');
-    // eslint-disable-next-line cypress/no-unnecessary-waiting
-    cy.wait(2000);
-  });
 });
