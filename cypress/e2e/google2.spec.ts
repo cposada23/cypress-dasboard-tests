bff6d1a21cd3aa2901879f6c727827dadbea3d25
camiloesub <105243064+camiloesub@users.noreply.github.com>
2022-07-03 00:22:40 -0500

    tooojgyfh

--- cypress/e2e/google2.spec.ts
+++ cypress/e2e/google2.spec.ts
@@ -4,7 +4,7 @@ describe('This is my first cypress test', () => {
     cy.title().should('eq', 'Google');
     cy.log(Cypress.env('CONTAINER') || 'NO_CONTAINER');
 
-    cy.get('[name="q"]').type(Cypress.env('CONTAINER') || 'NO_CONTAINER');
+    cy.get('[name="q"]').type(Cypress.env('container') || 'NO_CONTAINER');
     cy.get('[name="q"]').type(Cypress.env('NUMBER') || 'NO_NUMBER');
     cy.get('[name="q"]').type(Cypress.env('TEST') || 'NOTEST');
     cy.get('[name="q"]').type(Cypress.env('apiPort') || 'NOapiPort');
