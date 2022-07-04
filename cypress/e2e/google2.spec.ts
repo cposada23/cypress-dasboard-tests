7a09636ccbc8965834b7fd1ecb4e82b5607446f7
camiloesub <105243064+camiloesub@users.noreply.github.com>
2022-07-04 11:29:55 -0500

    ls with javascript ?

--- cypress/e2e/google2.spec.ts
+++ cypress/e2e/google2.spec.ts
@@ -4,7 +4,7 @@ describe('title', () => {
     cy.title().should('eq', 'Google');
     cy.log(Cypress.env('CONTAINER') || 'NO_CONTAINER');
     // eslint-disable-next-line cypress/no-unnecessary-waiting
-    cy.wait(10000);
+    cy.wait(5000);
     cy.get('[name="q"]').type(Cypress.env('container') || 'NO_CONTAINER');
     cy.get('[name="q"]').type(Cypress.env('NUMBER') || 'NO_NUMBER');
     cy.get('[name="q"]').type(Cypress.env('TEST') || 'NOTEST');
--- cypress/e2e/google3.spec.ts
+++ cypress/e2e/google3.spec.ts
@@ -2,7 +2,7 @@ describe('search id', () => {
   it('should have a title with failure', () => {
     cy.visit('https://www.google.com/');
     // eslint-disable-next-line cypress/no-unnecessary-waiting
-    cy.wait(10000);
+    cy.wait(5000);
     cy.title().should('eq', 'Google');
     cy.get('[name="q"]').type(Cypress.env('container') || 'NO_CONTAINER');
     cy.get('[name="q"]').type(Cypress.env('PROJECT_ID') || 'NO_PROJECT_ID');
